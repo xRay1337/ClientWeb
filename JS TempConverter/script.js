f83de6f917572d5c0ea7b6bca8cae8ee7f496232
Vitaliy Zlobin <zlovian@yandex.ru>
2020-07-07 18:47:57 +0700

    fix JS TempConverter#1

--- JS TempConverter/script.js
+++ JS TempConverter/script.js
@@ -1,17 +1,27 @@
-﻿var convertButton = document.getElementById("convert-button");
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var convertButton = document.getElementById("convert-button");
 
-convertButton.addEventListener("click", function (e) {
-    var converterInput = document.getElementById("converter-input");
-    var converterOutput = document.getElementById("converter-output");
-    var celsius = parseFloat(converterInput.value);
+    convertButton.addEventListener("click", function () {
+        var converterInput = document.getElementById("converter-input");
+        var converterOutput = document.getElementById("converter-output");
+        var celsius = parseFloat(converterInput.value);
 
-    if (isNaN(celsius)) {
-        converterOutput.value = "Invalid number";
-    } else {
-        var fahrenheit = celsius * 1.8 + 32;
-        var kelvin = celsius + 273.15;
+        if (isNaN(celsius)) {
+            converterOutput.style.color = "red";
+            converterOutput.value = "Invalid number";
+        } else {
+            converterOutput.style.color = "black";
 
-        converterOutput.value = "Celsius = " + celsius.toFixed(2) + "\n" + "Fahrenheit = " + fahrenheit.toFixed(2) + "\n" + "Kelvin = " + kelvin.toFixed(2);
-        converterInput.value = "";
-    }
+            var fahrenheit = celsius * 1.8 + 32;
+            var kelvin = celsius + 273.15;
+
+            converterOutput.value = "Celsius = " + celsius.toFixed(2) + "\n";
+            converterOutput.value += "Fahrenheit = " + fahrenheit.toFixed(2) + "\n";
+            converterOutput.value += "Kelvin = " + kelvin.toFixed(2);
+
+            converterInput.value = "";
+        }
+
+        converterInput.focus();
+    });
 });
