9c67891527585c2a100162216c5aa0d2f2b62642
Vitaliy Zlobin <zlovian@yandex.ru>
2020-07-28 00:04:40 +0700

    fix JS TodoList#2

--- JS TodoList/scripts/events.js
+++ JS TodoList/scripts/events.js
@@ -11,11 +11,11 @@
             newItem.innerHTML = "<div class='li-block'>\
                     <div class='li-note'>\
                         <span class='li-span'></span>\
-                        <input class='li-edit displayNone' type='text' maxlength='20' autofocus />\
+                        <input class='li-edit display-none' type='text' maxlength='20' autofocus />\
                     </div>\
                     <div>\
-                        <button class='button li-save-button displayNone' title='Save changes'><img src='images/save.png' alt='save'></button>\
-                        <button class='button li-cancel-button displayNone' title='Cancel changes'><img src='images/cancel.png' alt='cancel'></button>\
+                        <button class='button li-save-button display-none' title='Save changes'><img src='images/save.png' alt='save'></button>\
+                        <button class='button li-cancel-button display-none' title='Cancel changes'><img src='images/cancel.png' alt='cancel'></button>\
                         <button class='button li-edit-button' title='Edit'><img src='images/edit.png' alt='edit'></button>\
                         <button class='button li-delete-button' title='Delete'><img src='images/remove.png' alt='remove'></button>\
                     </div>\
@@ -29,16 +29,6 @@
             var liDeleteButton = newItem.querySelector(".li-delete-button");
             var liElements = [liSpan, liEdit, liSaveButton, liCancelButton, liEditButton, liDeleteButton];
 
-            function SwapDisplayNoneClass(elements) {
-                elements.forEach(function (element) {
-                    if (element.classList.contains("displayNone")) {
-                        element.classList.remove("displayNone");
-                    } else {
-                        element.classList.add("displayNone");
-                    }
-                });
-            }
-
             liSpan.textContent = text;
             liEdit.value = text;
 
@@ -48,18 +38,22 @@
             });
 
             liEditButton.addEventListener("click", function () {
-                SwapDisplayNoneClass(liElements);
+                swapDisplayNoneClass(liElements);
                 liEdit.focus();
             });
 
             liSaveButton.addEventListener("click", function () {
-                SwapDisplayNoneClass(liElements);
-                liSpan.textContent = liEdit.value;
+                swapDisplayNoneClass(liElements);
+
+                if (liEdit.value !== "") {
+                    liSpan.textContent = liEdit.value;
+                }
+
                 todoInput.focus();
             });
 
             liCancelButton.addEventListener("click", function () {
-                SwapDisplayNoneClass(liElements);
+                swapDisplayNoneClass(liElements);
                 liEdit.value = liSpan.textContent;
                 todoInput.focus();
             });
@@ -70,6 +64,16 @@
 
         todoInput.focus();
     });
+
+    function swapDisplayNoneClass(elements) {
+        elements.forEach(function (element) {
+            if (element.classList.contains("display-none")) {
+                element.classList.remove("display-none");
+            } else {
+                element.classList.add("display-none");
+            }
+        });
+    }
 
     document.getElementById("todo-input").addEventListener("keydown", function (event) {
         if (event.code === "Enter") {
